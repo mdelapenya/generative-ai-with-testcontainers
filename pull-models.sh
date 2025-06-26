ab1daae1a54857fcf9c46c7f4d0f04ed192328b8
Manuel de la Peña <mdelapenya@gmail.com>
2025-06-26 13:28:13 +0200

    feat: use DMR in 09-hugginface example

--- pull-models.sh
+++ pull-models.sh
@@ -3,5 +3,6 @@
 docker model pull ai/llama3.2:1B-Q4_0 &
 docker model pull ai/qwen3:0.6B-Q4_0 &
 docker model pull ai/mxbai-embed-large:335M-F16 &
+docker model pull hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF &
 
 wait
