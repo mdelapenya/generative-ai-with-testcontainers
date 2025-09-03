c8166fecb655ebb023af4bb62169f1531a479788
Manuel de la Peña <mdelapenya@gmail.com>
2025-09-03 18:49:38 +0200

    chore: bump ryuk image

--- pull-images.sh
+++ pull-images.sh
@@ -4,7 +4,7 @@ readonly OLLAMA_VERSION="0.11.8"
 
 docker pull ollama/ollama:${OLLAMA_VERSION}
 
-docker pull testcontainers/ryuk:0.11.0 &
+docker pull testcontainers/ryuk:0.12.0 &
 docker pull mdelapenya/moondream:${OLLAMA_VERSION}-1.8b &
 docker pull semitechnologies/weaviate:1.27.2 &
 docker pull pgvector/pgvector:pg16 &
